dfdfc15e065838d491d51b4f56641267d4258e1e
paulovargas <paulotomegomesdevargas@hotmail.com>
2018-04-11 21:33:12 -0300

    Adicionado Table Eventos dos Filhos
    tabela eventos filhos

---
+++ app/src/main/java/com/example/guilh/gerenciadordefilhos/tabelas/tableEventosDosFilhos.java
@@ -0,0 +1,31 @@
+package com.example.guilh.gerenciadordefilhos.tabelas;
+
+/**
+ * Created by guilh on 08/04/2018.
+ */
+
+public class tableEventosDosFilhos {
+
+    private static  final String TABLE = "eventos_dos_filhos";
+
+    private static final String CREATE =
+            "CREATE TABLE IF NOT EXISTS " + TABLE + " ( " +
+                    "filho_id int(10) unsigned NOT NULL," +
+                    "eventos_id int(10) unsigned NOT NULL," +
+                    "PRIMARY KEY ( filho_id , eventos_id)," +
+                    "KEY filho_has_eventos_FKIndex1 ( filho_id )," +
+                    "KEY filho_has_eventos_FKIndex2 ( eventos_id )," +
+                    "CONSTRAINT eventos_dos_filhos_ibfk_1 FOREIGN KEY (filho_id) REFERENCES filho ( id) ON DELETE NO ACTION ON UPDATE NO ACTION, " +
+                    "CONSTRAINT eventos_dos_filhos_ibfk_2 FOREIGN KEY (eventos_id) REFERENCES eventos (id) ON DELETE NO ACTION ON UPDATE NO ACTION " +
+                    ")";
+
+        private static final String DROP = "DROP TABLE IF EXISTS " + TABLE;
+
+        public String create() {
+            return CREATE;
+        }
+
+        public String upgrade() {
+            return DROP;
+        }
+}

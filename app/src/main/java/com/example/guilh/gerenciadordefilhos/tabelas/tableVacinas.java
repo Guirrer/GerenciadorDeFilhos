90ac3ed61f4efe94a780a5b299952eb200456052
Guirrer <guirrer@gmail.com>
2018-05-26 19:18:05 -0300

    Tablevacinas

--- app/src/main/java/com/example/guilh/gerenciadordefilhos/tabelas/tableVacinas.java
+++ app/src/main/java/com/example/guilh/gerenciadordefilhos/tabelas/tableVacinas.java
@@ -1,21 +1,44 @@
 package com.example.guilh.gerenciadordefilhos.tabelas;
 
-public class tableVacinas {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.annotation.NonNull;
+
+import com.example.guilh.gerenciadordefilhos.Util.Database;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class tableVacinas extends Database {
     private static  final String TABLE = "vacinas";
 
-    private Integer vacina_id;
+    private Integer id;
     private String nome_vacina;
     private Integer idade_minima;
     private Integer dose;
     private Float validade;
     private Float periodicidade;
 
+    private SQLiteDatabase db;
+    private Context context;
+
+    public tableVacinas(Context context) {
+        super(context);
+        this.context = context;
+        db = this.getWritableDatabase();
+    }
+
+
     public Integer getVacina_id() {
-        return vacina_id;
+        return id;
     }
 
     public void setVacina_id(Integer vacina_id) {
-        this.vacina_id = vacina_id;
+        this.id = vacina_id;
     }
 
     public String getNome_vacina() {
@@ -60,7 +83,7 @@ public class tableVacinas {
 
     private static final String CREATE =
             "CREATE TABLE IF NOT EXISTS " + TABLE + " ( " +
-                    "vacina_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, " +
+                    "id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT, " +
                     "nome_vacina VARCHAR(255) NOT NULL, " +
                     "idade_minima INT(11) UNSIGNED NULL DEFAULT NULL, " +
                     "dose INT(11) UNSIGNED NULL DEFAULT NULL, " +
@@ -74,4 +97,184 @@ public class tableVacinas {
     public String create() { return CREATE; }
 
     public String upgrade() { return DROP; }
+
+    public void select(int id)
+    {
+        String query = "SELECT * FROM " + TABLE + " WHERE ID = " + id ;
+        Cursor c = db.rawQuery(query, null);
+        if (c != null) {
+            c.moveToFirst();
+            if (c.getCount() > 0) {
+                this.id = c.getInt(c.getColumnIndex("id"));
+                this.nome_vacina = c.getString(c.getColumnIndex("nome_vacina"));
+                this.idade_minima = c.getInt(c.getColumnIndex("idade_minima"));
+                this.dose = c.getInt(c.getColumnIndex("dose"));
+                this.validade = c.getFloat(c.getColumnIndex("validade"));
+                this.periodicidade = c.getFloat(c.getColumnIndex("periodicidade"));
+            }
+        }
+    }
+
+    public void selectList()
+    {
+        String query = "SELECT * FROM " + TABLE + " WHERE ID = " + id ;
+        Cursor c = db.rawQuery(query, null);
+        List<tableVacinas> list = new List<tableVacinas>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public Iterator<tableVacinas> iterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @NonNull
+            @Override
+            public <T> T[] toArray(@NonNull T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(tableVacinas tableVacinas) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(@NonNull Collection<? extends tableVacinas> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(int index, @NonNull Collection<? extends tableVacinas> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(@NonNull Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public tableVacinas get(int index) {
+                return null;
+            }
+
+            @Override
+            public tableVacinas set(int index, tableVacinas element) {
+                return null;
+            }
+
+            @Override
+            public void add(int index, tableVacinas element) {
+
+            }
+
+            @Override
+            public tableVacinas remove(int index) {
+                return null;
+            }
+
+            @Override
+            public int indexOf(Object o) {
+                return 0;
+            }
+
+            @Override
+            public int lastIndexOf(Object o) {
+                return 0;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<tableVacinas> listIterator() {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public ListIterator<tableVacinas> listIterator(int index) {
+                return null;
+            }
+
+            @NonNull
+            @Override
+            public List<tableVacinas> subList(int fromIndex, int toIndex) {
+                return null;
+            }
+        };
+        if (c != null) {
+            c.moveToFirst();
+            if (c.getCount() > 0) {
+                tableVacinas table = new tableVacinas(this.context);
+                table.id = c.getInt(c.getColumnIndex("id"));
+                table.nome_vacina = c.getString(c.getColumnIndex("nome_vacina"));
+                table.idade_minima = c.getInt(c.getColumnIndex("idade_minima"));
+                table.dose = c.getInt(c.getColumnIndex("dose"));
+                table.validade = c.getFloat(c.getColumnIndex("validade"));
+                table.periodicidade = c.getFloat(c.getColumnIndex("periodicidade"));
+                list.add(table);
+            }
+        }
+    }
+
+    public long insert()
+    {
+        return db.insert(TABLE, null, retornaValues());
+    }
+
+    public int update()
+    {
+        return db.update(TABLE, retornaValues(), "id = ?", new String[]{ this.id.toString() });
+    }
+    private ContentValues retornaValues()
+    {
+        ContentValues values = new ContentValues();
+
+        values.put("nome_vacina", this.nome_vacina);
+        values.put("idade_minima", this.idade_minima);
+        values.put("dose", this.dose);
+        values.put("validade", this.validade);
+        values.put("periodicidade", this.periodicidade);
+
+        return  values;
+    }
 }

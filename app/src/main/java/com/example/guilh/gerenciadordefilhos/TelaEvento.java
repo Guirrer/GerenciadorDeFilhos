20054852549f7dab74ae9f132d4b20a83cb2c7e9
dionatanmsantos <dionatanmsantos@gmail.com>
2018-07-01 16:48:03 -0300

    Ligação Telas

--- app/src/main/java/com/example/guilh/gerenciadordefilhos/TelaEvento.java
+++ app/src/main/java/com/example/guilh/gerenciadordefilhos/TelaEvento.java
@@ -1,14 +1,29 @@
 package com.example.guilh.gerenciadordefilhos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaEvento extends AppCompatActivity {
+
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_evento);
+
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaEvento.this, CadastroEvento.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/guilh/gerenciadordefilhos/TelaVacinacao.java
+++ app/src/main/java/com/example/guilh/gerenciadordefilhos/TelaVacinacao.java
@@ -1,14 +1,28 @@
 package com.example.guilh.gerenciadordefilhos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaVacinacao extends AppCompatActivity {
+
+    private Button btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_vacinacao);
 
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TelaVacinacao.this, CadastroVacinas.class);
+                startActivity(intent);
+            }
+        });
     }
 }
